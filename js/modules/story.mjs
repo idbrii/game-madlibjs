c43fef259fe94502dae1ee6e4193e44251943ac4
David Briscoe <idbrii@gmail.com>
2022-02-26 13:00:01 -0800

    Add some content validation

--- js/modules/story.mjs
+++ js/modules/story.mjs
@@ -1,5 +1,8 @@
 
 export const Word = function(kind, hint, name) {
+    console.assert(kind);
+    console.assert(hint);
+    console.assert(name);
     return {
         kind: kind,
         hint: hint,
@@ -7,7 +10,26 @@ export const Word = function(kind, hint, name) {
     };
 }
 
+const countWordOccurences = function(list) {
+    return list.reduce((acc, w) => {
+        const curr = w.name;
+        acc[curr] ??= 0;
+        ++acc[curr];
+        return acc;
+    }, {});
+}
+
 export const Story = function(title, words, text_fn) {
+    console.assert(title);
+    console.assert(words);
+    console.assert(text_fn);
+    const counts = countWordOccurences(words);
+    for (let k in counts) {
+        const count = counts[k];
+        if (count > 1) {
+            console.assert(false, `Word '${k}' was used ${count} times.`);
+        }
+    }
     return {
         title: title,
         words: words,
